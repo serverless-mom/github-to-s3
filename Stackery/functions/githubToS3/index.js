25c2d265698657ca1be3ece0d6ca2ad0c15cf24c
Toby Fee <TobyFee@gmail.com>
2018-05-08 12:03:30 -0700

    add first lambda code

--- Stackery/functions/githubToS3/index.js
+++ Stackery/functions/githubToS3/index.js
@@ -1,5 +1,29 @@
-module.exports = function handler(message, context, callback) {
-  console.dir(message);
+const AWS = require('aws-sdk');
+const fs = require('fs')
+const fileName = 'hello.html'
 
-  callback(null, {});
-}
+const s3 = new AWS.S3();
+
+module.exports = function handler(event, context, callback) {
+    console.log(event);
+    const ports = JSON.parse(process.env.STACKERY_PORTS)
+    fs.readFile(`./${fileName}`, 'utf8', function(err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      let params = {
+        Body: data,
+        Key: `${fileName}`,
+        Bucket: ports[0][0].bucket
+      };
+      console.log(data);
+      s3.putObject(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          callback(null, {});
+        } else {
+          console.dir(data);
+          callback(null, {});
+        }
+      })
+    });
