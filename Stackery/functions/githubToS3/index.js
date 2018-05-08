07efaa24195d64ea7aeef02fe5a6cf3dfe236a25
Toby Fee <TobyFee@gmail.com>
2018-05-08 16:35:45 -0700

    respond correctly to getting called on start

--- Stackery/functions/githubToS3/index.js
+++ Stackery/functions/githubToS3/index.js
@@ -1,30 +1,33 @@
 const AWS = require('aws-sdk');
 const fs = require('fs')
 const fileName = 'hello.html'
+const cfnCR = require('cfn-custom-resource');
+
 
 const s3 = new AWS.S3();
 
-module.exports = function handler(event, context, callback) {
-    console.log(event);
-    const ports = JSON.parse(process.env.STACKERY_PORTS)
-    fs.readFile(`./${fileName}`, 'utf8', function(err, data) {
+module.exports = async event {
+  console.dir(event);
+  const ports = JSON.parse(process.env.STACKERY_PORTS)
+  fs.readFile(`./${fileName}`, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+      await cfnCR.sendFailure(err.message, event);
+    }
+    let params = {
+      Body: data,
+      Key: `${fileName}`,
+      Bucket: ports[0][0].bucket
+    };
+    console.log(data);
+    s3.putObject(params, (err, data) => {
       if (err) {
-        return console.log(err);
+        console.log(err);
+        await cfnCR.sendFailure(err.message, event);
+      } else {
+        console.dir(data);
+        await cfnCR.sendFailure(err.message, event);
       }
-      let params = {
-        Body: data,
-        Key: `${fileName}`,
-        Bucket: ports[0][0].bucket
-      };
-      console.log(data);
-      s3.putObject(params, (err, data) => {
-        if (err) {
-          console.log(err);
-          callback(null, {});
-        } else {
-          console.dir(data);
-          callback(null, {});
-        }
-      })
-    });
+    })
+  });
 }
