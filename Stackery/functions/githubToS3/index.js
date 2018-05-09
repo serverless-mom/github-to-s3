eaeba63d4d4a7e592972ff0c4138c36e4f7c17e3
Toby Fee <TobyFee@gmail.com>
2018-05-09 13:32:31 -0700

    correct formatting

--- Stackery/functions/githubToS3/index.js
+++ Stackery/functions/githubToS3/index.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
-const fs = require('fs')
-const fileName = 'hello.html'
+const fs = require('fs');
+const fileName = 'hello.html';
 const cfnCR = require('cfn-custom-resource');
 
 
 const s3 = new AWS.S3();
 
-module.exports = async event {
+module.exports = async event => {
   console.dir(event);
   const ports = JSON.parse(process.env.STACKERY_PORTS)
   let data = fs.readFileSync(`./${fileName}`, 'utf8')
