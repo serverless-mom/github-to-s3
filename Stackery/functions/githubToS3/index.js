09e8ad4b222adb1967e5e5c55b0b79667eb6a8b2
Toby Fee <TobyFee@gmail.com>
2018-05-10 13:44:57 -0700

    added requirement

--- Stackery/functions/githubToS3/index.js
+++ Stackery/functions/githubToS3/index.js
@@ -1,27 +1,11 @@
-const AWS = require('aws-sdk');
-const fs = require('fs');
-const fileName = 'hello.html';
+var aws = require('aws-sdk');
+var lambda = new aws.Lambda({
+  region: 'us-west-2'
+});
 const cfnCR = require('cfn-custom-resource');
 
 
-const s3 = new AWS.S3();
-
 module.exports = async event => {
-  console.dir(event);
-  const ports = JSON.parse(process.env.STACKERY_PORTS)
-  let data = fs.readFileSync(`./${fileName}`, 'utf8')
-  let params = {
-    Body: data,
-    Key: `${fileName}`,
-    Bucket: ports[0][0].bucket
-  };
-  s3.putObject(params, (err, data) => {
-    if (err) {
-      console.log(err);
-      await cfnCR.sendFailure(err.message, event);
-    } else {
-      console.dir(data);
-      await cfnCR.sendSuccess('s3-copy', {}, event);
-    }
-  })
-}
+  console.log('gatekeeper active')
+  return cfnCR.sendSuccess('start', {}, event);
+};
